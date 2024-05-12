19eac78fdfb7eb0c3f678e42b55e0e130316a9e2
Ahmed-kotb621 <akotb621@gmail.com>
2024-05-12 22:51:58 +0300

    error handler

--- server/handlers/postHandlers.ts
+++ server/handlers/postHandlers.ts
@@ -4,10 +4,14 @@ import { Post } from "../types";
 import crypto from "crypto";
 
 export const listPostHandler: RequestHandler = (req, res) => {
+  // throw new Error("oooops!");
   res.send({ posts: db.listPosts() });
 };
 
 export const createPostHandler: RequestHandler = (req, res) => {
+  if (!req.body.title || !req.body.url || !req.body.userId) {
+    return res.sendStatus(400);
+  }
   const post: Post = {
     id: crypto.randomUUID(),
     title: req.body.title,
--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express, { RequestHandler, request } from "express";
+import express, { ErrorRequestHandler, RequestHandler, request } from "express";
 import { db } from "./datastore";
 import { createPostHandler, listPostHandler } from "./handlers/postHandlers";
 const app = express();
@@ -15,7 +15,15 @@ const loggerMiddleWare: RequestHandler = (req, res, next) => {
 app.use(loggerMiddleWare);
 
 // endpoint get posts => after hostname and port number execute function
-app.get("/posts", listPostHandler);
-app.post("/posts", createPostHandler);
+app.get("/v1/posts", listPostHandler);
+app.post("/v1/posts", createPostHandler);
+// comments endpoints
+
+// Error Hanlder middle ware
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  return res.status(500).send("Ooops Error in server side, plz try again !");
+};
+
+app.use(errorHandler);
 
 app.listen(3000);
